db4499a1446808cc5732423681d65886c0aba53c
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-07-24 09:10:19 +0300

    Add socks builder

--- ktor-client/ktor-client-core/posix/src/io/ktor/client/engine/ProxyConfigNative.kt
+++ ktor-client/ktor-client-core/posix/src/io/ktor/client/engine/ProxyConfigNative.kt
@@ -6,6 +6,8 @@ import io.ktor.http.*
  * Proxy configuration.
  *
  * See [ProxyBuilder] to create proxy.
+ *
+ * @param url: proxy url address.
  */
 actual class ProxyConfig(val url: Url) {
     override fun toString(): String = url.toString()
@@ -16,7 +18,17 @@ actual class ProxyConfig(val url: Url) {
  */
 actual object ProxyBuilder {
     /**
-     * Create http proxy from url.
+     * Create http proxy from [url].
      */
     actual fun http(url: Url): ProxyConfig = ProxyConfig(url)
+
+    /**
+     * Create socks proxy from [host] and [port].
+     */
+    actual fun socks(host: String, port: Int): ProxyConfig = ProxyConfig(URLBuilder().apply {
+        protocol = URLProtocol.SOCKS
+
+        this.host = host
+        this.port = port
+    }.build())
 }
